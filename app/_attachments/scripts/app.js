af71c95929982146f033eacbacdf0fa2f40b4454
BierinckxNicolas <nicolas@Nicolass-MacBook-Pro.local>
2017-11-08 16:56:30 +0100

    Movielijst fixed
    Update + screenshot

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -22,8 +22,14 @@ angular.module('movieApp', ['ngRoute'])
 	    		var name = $('#nameText').val();
 	    		
 	    		movieSrv.getMovies(name).then(function(data){
-	    		$scope.movies = data;
-		    
+	    			
+	    			var arr = data.data[0].filmography.actor.map(function(a, b){
+	    				   return a.title;
+	    				});
+	    				$scope.movies = arr;
+	    			
+	    			//$scope.movies = data.data[0].filmography.actor;				
+	    		
 	    		});
 	    	});
     })
@@ -44,69 +50,7 @@ angular.module('movieApp', ['ngRoute'])
 	    		};
     })
     
-    .service('zoneSrv', function($http, $q) {
-    		this.getZones = function() {
-			var q = $q.defer();
-			$http.get('http://datasets.antwerpen.be/v4/gis/paparkeertariefzones.json')
-				.then(function(data, status, headers, config){
-					q.resolve(data.data);
-				}, function error(err) {
-					q.reject(err);
-				});
-			
-			return q.promise;
-		};
-		
-	// http://alienryderflex.com/polygon/
-		this.inPolygon = function(location, polyLoc){
-			var lastPoint = polyLoc[polyLoc.length-1];
-			var isInside = false;
-			var x = location[0];
-
-			for(var i = 0; i < polyLoc.length; i++){
-				var point = polyLoc[i];
-				var x1 = lastPoint[0];
-				var x2 = point[0];
-				var dx = x2 - x1;
-
-				if(Math.abs(dx) > 180.0){
-					if(x > 0){
-						while(x1 < 0)
-							x1 += 360;
-						while(x2 < 0)
-							x2 += 360;
-					}
-					else{
-						while(x1 > 0)
-							x1 -= 360;
-						while(x2 > 0)
-							x2 -= 360;
-					}
-					dx = x2 - x1;
-				}
-
-				if((x1 <= x && x2 > x) || (x1 >= x && x2 < x)){
-					var grad = (point[1] - lastPoint[1]) / dx;
-					var intersectAtLat = lastPoint[1] + ((x - x1) * grad);
-
-					if(intersectAtLat > location[1])
-						isInside = !isInside;
-				}
-				lastPoint = point;
-			}
-			return isInside;
-		};
-		
-		this.getTariff = function(lng, lat, zones){
-			for(var i = 0; i < zones.length; i++) {
-				var geo = JSON.parse(zones[i].geometry);
-				var coordinates = geo.coordinates[0];
-				if(this.inPolygon([lng, lat], coordinates)) {
-					return zones[i].tariefkleur;
-				}
-			}
-		};
-    })
+   
     
     .service('saveSrv', function($window, $http){
 		  this.setObject = function(key, value){
